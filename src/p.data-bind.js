bb7ba0ca78f873358b2812785ae3cd71bc2ad297
educastellano <educastellano08@gmail.com>
2013-02-19 19:33:04 +0100

    p.data-bind recursively binds dom elements

--- src/p.data-bind.js
+++ src/p.data-bind.js
@@ -1,49 +1,57 @@
 'use strict';
 
+P.databind = {};
 
-P.View.onModelChange = function onModelChange (e, args) {
+P.databind.onModelChange = function onModelChange (e, args) {
+    var i,
+        el;
+
+    try {
+        for (i=0; i<this.el.length; i++) {
+            el = $(this.el.get(i));
+            P.databind.bindData(this, el, args);
+        }
+    }
+    catch (e) {
+
+    }
+};
+
+P.databind.bindData = function bindData (view, el, args) {
     var bind,
         aux,
         attrDOM,
         attr,
         el,
         i,
-        value;
+        value,
+        children;
 
-    //
-    // TODO check for recursively tags
+    if (el) {
+        bind = el.data().bind;
+        if (bind) {
+            aux = bind.split(':');
+            attrDOM = aux[0];
+            attr = aux[1];
+            if (attr === args.attr) {
+                if (view.defViewModel && view.defViewModel[attr]) {
+                    value = view.defViewModel[attr](args.value);
+                }
+                else {
+                    value = args.value;
+                }
 
-    try {
-        for (i=0; i<this.el.length; i++) {
-            el = $(this.el.get(i));
-            bind = el.data().bind;
-
-            if (bind) {
-                aux = bind.split(':');
-                attrDOM = aux[0];
-                attr = aux[1];
-                if (attr === args.attr) {
-                    debugger;
-
-                    if (this.defViewModel && this.defViewModel[attr]) {
-                        value = this.defViewModel[attr](args.value);
-                    }
-                    else {
-                        value = args.value;
-                    }
-
-                    if (attrDOM === 'text' || attrDOM === 'html') {
-                        el[attrDOM](value);
-                    }
-                    else {
-                        el.attr(attrDOM, value);
-                    }
+                if (attrDOM === 'text' || attrDOM === 'html') {
+                    el[attrDOM](value);
+                }
+                else {
+                    el.attr(attrDOM, value);
                 }
             }
         }
+        children = el.children();
+        for (i=0; i<children.length; i++) {
+            bindData(view, $(children.get(i)), args);
+        }
     }
-    catch (e) {
-
-    }
-
 };
