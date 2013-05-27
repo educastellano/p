23eb9c08b3873a147fedd5b6050ec0dc20deb671
educastellano <educastellano08@gmail.com>
2013-05-27 12:03:29 +0200

    "lib" folder renamed to "dist". Adding versioning of views and persitence strategies

---
+++ dist/persist/p.rest-0.0.0.js
@@ -0,0 +1,65 @@
+'use strict';
+
+P.persistREST = function (method, model, options) {
+    var methodMap = model.methodMap || P.methodMap,
+        type = methodMap[method],
+        params = {
+            type: type,
+            dataType: 'json'
+        },
+        opt,
+        data,
+        api;
+
+    options = options || {};
+
+    if (P.List.isPrototypeOf(model) && model.model) {
+        // list url prevals over model url
+        if (!model.url) {
+            model = model.model;
+        }
+    }
+
+    // Set data for updates
+    if (method === 'create' || method === 'update') {
+        data = options.data || (method === 'create' ? model.attr : model.changed);
+        if (data && Object.keys(data).length) {
+            params.contentType = 'application/json';
+            params.data = model.toDataAPI(data, method);
+        }
+    }
+
+    // Don't process data on a non-GET request.
+    if (params.type !== 'GET') {
+        params.processData = false;
+    }
+
+    // Override params with the user options
+    for (opt in options) {
+        if (options.hasOwnProperty(opt)) {
+            params[opt] = options[opt];
+        }
+    }
+
+    // Set URL
+    api = model.api || model.url;
+    if (typeof api === 'object') {
+        params.url = api[method];
+    }
+    else if (typeof api === 'function') {
+        params.url = api.call(model, options.urlParams, method);
+        model.urlParams = options.urlParams;
+        delete params.urlParams;
+    }
+    else { // string. RESTful url
+        params.url = api;
+        if (method !== 'create') {
+            if (params.url[params.url.length-1] !== '/') {
+                params.url += '/';
+            }
+            params.url += model.getId();
+        }
+    }
+
+    return $.ajax(params);
+};
